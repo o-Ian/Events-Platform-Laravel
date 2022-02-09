04c1295b704cd79bd714cc0278d551d34fb46fea
Ian <ianstigli@hotmail.com>
2022-02-09 01:14:42 +0000

    layout and pages(initial) develpoment

---
+++ resources/views/events/create/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.site')
+
+@section('title', 'Debian - Produtos')
+
+@section('content')
+
+<h1>Crie um evento</h1>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\EventController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [EventController::class, 'index'])->name('site.home');
+
+Route::get('events/create', [EventController::class, 'create'])->name('site.create');
